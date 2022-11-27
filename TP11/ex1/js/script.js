75c2d546043a352685a61a7d5557394bd3e69203
Fredray-21 <dabadie.frederic@gmail.com>
2022-11-27 23:01:56 +0100

    ajout switch formulaire cnx/insc

--- TP11/ex1/js/script.js
+++ TP11/ex1/js/script.js
@@ -23,11 +23,23 @@ function openModal(objet, id, cle) {
 function closeModal() {
     modal.style.display = "none";
     document.documentElement.style.overflow = 'auto';
-}
-;
-const listeObjets = document.querySelectorAll("#results .ligne");
+};const listeObjets = document.querySelectorAll("#results .ligne");
 
 // add animation fade in in différente vitesse
 for (let i = 0; i < listeObjets.length; i++) {
     listeObjets[i].style.animation = "fadein 1s " + (i / 10) + "s forwards";
 }
+
+const switch_home = document.querySelector("#switch-home");
+const form_connexion = document.querySelector("#form-connexion");
+const form_inscription = document.querySelector("#form-inscription");
+
+switch_home.addEventListener("click", function () {
+    if (switch_home.checked) {
+        form_connexion.style.display = "none";
+        form_inscription.style.display = "block";
+    } else {
+        form_connexion.style.display = "block";
+        form_inscription.style.display = "none";
+    }
+});
